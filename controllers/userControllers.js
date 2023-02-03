2af49157a60d4d36f78cc12e60478ac807ed4814
Facundo Noya <facunoya@hotmail.com>
2023-02-03 13:41:26 -0300

    agregando bcryptjs

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,21 +1,23 @@
 const fs = require('fs')
 const path = require('path')
 const user = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/user.json'), 'utf-8'))
+const bcrytjs = require('bcryptjs')
 
 const userControllers = {
     getRegister: (req, res) =>{
         res.render('register')
     },
     generateId: () => {
-        const allUsers  = user
+        const allUsers  = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/user.json'), 'utf-8'))
         
-        if( allUsers === [""]){ return 1 } else { let nuevoId = allUsers.pop(); 
+        if( allUsers.length < 1){ return 1 } else { let nuevoId = allUsers.pop(); 
             return nuevoId.id + 1 } 
     },
     register: (req, res) => {
         let newUser = {
             id: userControllers.generateId(),
-            ...req.body 
+            ...req.body, 
+            password: bcrytjs.hashSync(req.body.password, 10)
         }
         user.push(newUser)
         fs.writeFileSync(path.join(__dirname, '../data/user.json'), JSON.stringify(user, null, " "))
